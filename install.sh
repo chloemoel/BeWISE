0532e416eaa7704a89fe6380a87f3c4586017a58
Chloe Moel (Beck) <corrychlois@gmail.com>
2026-08-13 13:20:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 mkdir -p bin/data
 cd bin/data
 
-wget https://github.com/chloemoel/MagicWiseIndex/releases/download/v.0.0.2/probe_info.csv.gz
+wget https://github.com/chloemoel/BeWISE/releases/download/v.0.0.2/probe_info.csv.gz
 echo "Unzipping methylation probe info..."
-tar -xvf probe_info.csv.gz
+gunzip probe_info.csv.gz
 
 echo "All done!"
